a6e7ea871b9f0fd16c460dd6a95b799c695d971f
Puchta Dawid <pd57883>
2026-06-01 15:04:56 +0200

    lab-h finished

---
+++ lab-h/src/Entity/Book.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $author = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $isbn = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $publication_year = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getIsbn(): ?string
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn(?string $isbn): static
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getPublicationYear(): ?int
+    {
+        return $this->publication_year;
+    }
+
+    public function setPublicationYear(?int $publication_year): static
+    {
+        $this->publication_year = $publication_year;
+
+        return $this;
+    }
+}
